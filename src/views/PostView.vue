ceb8ae1ee49a6fa29af4e3079272079a5cc1620e
Popa-Andrei-Valentin <andrei_popa2009@icloud.com>
2023-04-02 18:40:36 +0300

    Post page prototyping

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>Test test</h1>
-  </div>
+  <v-container>
+    <v-col>
+      <v-btn color="blue" @click="goToHome">
+        <v-icon start icon="mdi-arrow-left" />
+        Back
+      </v-btn>
+    </v-col>
+
+    <v-row class="fill-height overflow-y-auto">
+      <v-col align="center" class="fill-height"><h1>test</h1></v-col>
+    </v-row>
+
+    <v-row class="fill-height overflow-y-auto ma-2">
+      <v-col align="start" class="fill-height">
+        <p>Test test</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import router from '@/router'
+
+export default {
+  name: 'PostView',
+
+  methods: {
+    goToHome () {
+      router.push('/')
+    }
+  }
+}
+</script>
